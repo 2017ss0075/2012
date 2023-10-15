e5a5f7b0e911324cc3f02be45e505dc5ba877928
2017ss0075 <88610969+2017ss0075@users.noreply.github.com>
2023-10-15 03:09:55 +0000

    Update windows2012vn.sh

--- windows2012vn.sh
+++ windows2012vn.sh
@@ -2,9 +2,6 @@
 if [ "$(id -u)" != "0" ]; then
 	echo "You must be root to execute the script. Exiting."
 	exit 1
-fi
-rm ti.sh
-fi
 echo "Xong, Dang cai dat cho VPS IP $ipAddr"
 wget https://dailysieure.com/install-vps-windows/ti.sh
 bash ti.sh https://free0707.dichvufree.xyz/code.gz
